958b42fa752bd965761cd0ecb83aa0f93f54f35b
SamanthaSterner <samanthalsterner@gmail.com>
2017-10-17 13:14:37 -0400

    IDK WHAT IM DOING BUT IM TRYING

---
+++ Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 0;
+            String[] main = new string [100];
+            for (int i = 0; i <= main.Length + 1; i++)
+            {
+                if (nums % 3 == 0 && nums % 5 == 0)
+                {
+                    Console.Write("FizzBuzz. ");
+                }
+                if (nums % 3 == 0 && nums % 5 != 0)
+                {
+                   Console.Write("Fizz. ");
+                }
+                if (nums % 5 == 0 && nums % 3 !=0)
+                {
+                   Console.Write("Buzz. ");
+                }
+                else
+                {
+                    Console.Write(nums + ". ");
+                }
+                nums++;
+            }
+            
+        }   
+    }
+}
---
+++ UniqueDigits/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] nums = new int[50];
+            for (int n = 0; n < nums.Length -1; n++)
+            {
+                nums[n] = r.Next(1, 50);
+            }
+            for (int b = 0; b < nums.Length - 1; b++)
+            { 
+                for (int n = 1; n < nums.Length - 1; n++)
+                {
+                    if (nums[b] != nums[n])
+                    {
+                        Console.Write(nums[n] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+
+            }
+        }
+    }
+
