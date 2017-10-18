d1b9bcc8608b139a1699f7a81bd848d06d39e487
SamanthaSterner <samanthalsterner@gmail.com>
2017-10-18 11:24:31 -0400

    IT WORKED!
    #boolvars

--- UniqueDigits/Program.cs
+++ UniqueDigits/Program.cs
@@ -12,23 +12,24 @@ namespace UniqueDigits
         {
             Random r = new Random();
             int[] nums = new int[50];
+            bool check = true;
             for (int n = 0; n < nums.Length -1; n++)
             {
-                nums[n] = r.Next(1, 50);
+                nums[n] = r.Next(2, 50);
             }
-            for (int b = 0; b < nums.Length - 1; b++)
-            { 
-                for (int n = 1; n < nums.Length - 1; n++)
+            for (int b = 0; b < nums.Length; b++)
+            {
+                check = true;
+                for (int a = 0; a < nums.Length; a++)
                 {
-                    if (nums[b] != nums[n])
+                    if (nums[b] == nums[a] && a != b)
                     {
-                        Console.Write(nums[n] + " ");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
+                        check = false;
                     }
                 }
+
+                if (check)
+                    Console.Write(nums[b] + " ");
             }
 
             }
