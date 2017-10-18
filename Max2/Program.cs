32f8d65507282c780317a1a4991dc29c9ce8740d
SamanthaSterner <samanthalsterner@gmail.com>
2017-10-18 12:11:42 -0400

    Max2 IS LIT!
    I think this is right but prob not but claps bc i tried :)

---
+++ Max2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] nums = new int[5000];
+            for (int n = 0; n < nums.Length - 1; n++)
+            {
+                nums[n] = r.Next(1, 1000000);
+            }
+            int[] change = nums.OrderByDescending(c => c).ToArray();
+            Console.Write(change[0] + " " + change[1]);
+        }
+    }
+}
