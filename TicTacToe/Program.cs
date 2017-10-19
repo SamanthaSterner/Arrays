a9cc4d6e4731ddc6e8806c7c4ad4a7d164dbc5d3
SamanthaSterner <samanthalsterner@gmail.com>
2017-10-19 11:32:18 -0400

    TicTacToe start

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] board = new string[3, 3];
+            for (int a = 0; a < board.Length; a ++)
+            {
+                for (int b = 0; b < board.Length; b++)
+                {
+                    board[a , b] = "+---\n\n|";
+                }
+            }
+            for (int c = 0; c < board.Length; c++)
+            {
+                for(int d = 0; d < board.Length; d++)
+                {
+                    Console.Write(board[c, d]);
+                }
+            }
+        }
+    }
+}
