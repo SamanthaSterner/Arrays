995138b78ad41efd6d8afb5942d475b7b3af1b25
SamanthaSterner <samanthalsterner@gmail.com>
2017-10-19 11:08:56 -0400

    Dedupes Done!!
    Im on a ROLLLLLLLLLL!

---
+++ Dedupe/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dedupe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Random r = new Random();
+            int[] nums = new int[5000];
+            for (int n = 0; n < nums.Length - 1; n++)
+            {
+                nums[n] = r.Next(1, 50);
+            }
+            int[] change = nums.OrderByDescending(c => c).ToArray();
+            for (int b = 0; b < nums.Length; b++)
+            {
+                for (int a = 0; a < nums.Length; a++)
+                {
+                    if (nums[b] == nums[a] && a != b)
+                    {
+                        nums[a] = -1;
+                        nums[b] = -1;
+
+                    }
+                }
+                if(nums[b] != -1)
+                {
+                    count++;
+                }
+            }
+            Console.Write(count);
+        }
+    }
+}
